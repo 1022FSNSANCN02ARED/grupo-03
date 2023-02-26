2a0df2b45089067e21e5048c8399495f265c45e7
FedeVazquez <fedenv2009@hotmail.com>
2023-02-26 12:38:12 -0300

    Instala express-session y lo configura en app.js.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
+const session = require("express-session");
+
 app.listen(3000, () => console.log("servidor corrinedo en el puerto 3000"));
 
 const publicPath = path.join(__dirname, "../public");
@@ -13,6 +15,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./views"));
 
 app.use(express.urlencoded({ extended: false }));
+
+app.use(session({ secret: "Un mensaje secreto" }));
 
 const mainRouter = require("./routers/mainRouter");
 app.use(mainRouter);
