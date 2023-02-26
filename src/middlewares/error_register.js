79ac4c60d798244b8024993e0cecf460b95f829b
FedeVazquez <fedenv2009@hotmail.com>
2023-02-26 12:07:59 -0300

    Borra la contraseña y la confirmación de la contraseña antes de renderizar el register cuando hay un error en el formulario.

--- src/middlewares/error_register.js
+++ src/middlewares/error_register.js
@@ -3,6 +3,9 @@ const { validationResult } = require("express-validator");
 module.exports = (req, res, next) => {
     let errores = validationResult(req);
     if (!errores.isEmpty()) {
+        delete req.body.password;
+        delete req.body.confirmPassword;
+        res.send(req.body);
         res.render("register", {
             errores: errores.mapped(),
             oldData: req.body,
