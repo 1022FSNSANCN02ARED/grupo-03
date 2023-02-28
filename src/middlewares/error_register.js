8c89cf34375db025019323f76db0bb58ba67f601
FedeVazquez <fedenv2009@hotmail.com>
2023-02-28 16:08:59 -0300

    Crea un .custom en la contraseña y confirmación para generar un mensaje de error en caso de que sean distintas. Y coloca mensajes de los errores que estan teniendo los inputs al enviar el formulario, para que el ususario sepa por que se rechazo el registro.

--- src/middlewares/error_register.js
+++ src/middlewares/error_register.js
@@ -3,11 +3,8 @@ const { validationResult } = require("express-validator");
 module.exports = (req, res, next) => {
     let errores = validationResult(req);
     if (!errores.isEmpty()) {
-        delete req.body.password;
-        delete req.body.confirmPassword;
-        res.send(req.body);
         res.render("register", {
-            errores: errores.mapped(),
+            errors: errores.mapped(),
             oldData: req.body,
         });
     } else {
--- src/middlewares/registerMiddleware.js
+++ src/middlewares/registerMiddleware.js
@@ -14,14 +14,24 @@ module.exports = [
         .withMessage("Debe introducir una contraseña.")
         .bail()
         .isLength({ min: 6 })
-        .withMessage("La contraseña debe tener como mínimo 6 caracteres."),
+        .withMessage("La contraseña debe tener como mínimo 6 caracteres.")
+        .bail()
+        .custom((password, { req }) => {
+            if (password != req.body.confirmPassword) {
+                console.log(req.body.confirmPassword);
+                throw new Error("Las contraseñas no coinciden.");
+            }
+            return true;
+        }),
     body("confirmPassword")
         .notEmpty()
-        .withMessage("Debe introducir la confirmación de la contraseña.")
+        .withMessage("Debe introducir la confirmación de la contraseña")
         .bail()
         .custom((confirmPassword, { req }) => {
-            if (confirmPassword !== req.body.password) {
-                throw new Error("Las contraseñas no son iguales.");
+            if (confirmPassword != req.body.password) {
+                console.log(req.body.password);
+                throw new Error("Las contraseñas no coinciden.");
             }
+            return true;
         }),
 ];
