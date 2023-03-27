c048ed1494abee70d101e683bc506e1e73dd9107
FedeVazquez <fedenv2009@hotmail.com>
2023-03-27 14:53:04 -0300

    Agrega modelo de usuarios.

---
+++ src/database/models/users.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, DataTypes) => {
+    const model = sequelize.define(
+        "Users",
+        {
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            phone_number: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+            password: { type: DataTypes.TEXT, allowNull: false },
+            first_name: { type: DataTypes.STRING, allowNull: false },
+            last_name: { type: DataTypes.STRING, allowNull: false },
+            avatar: { type: DataTypes.STRING, allowNull: true },
+        },
+        {
+            tableName: "users",
+        }
+    );
+
+    model.associate = (db) => {
+        model.belongsTo(db.Address, {
+            as: "address",
+            foreignKey: "address_id",
+        });
+        model.belongsTo(db.Roles, {
+            as: "rol",
+            foreignKey: "rol_id",
+        });
+    };
+
+    return model;
+};
