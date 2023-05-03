47ca4c680e8e64690016a364f9d7a93e8fbd0579
ivan ramirez <ivanramirez569@gmail.com>
2023-05-03 20:52:15 -0300

    Crea la API de Activity

---
+++ src/controllers/api/activityApiControllers.js
@@ -0,0 +1,36 @@
+const { Activities } = require("../../database/models");
+module.exports = {
+    async findActivities(req, res) {
+        const data = await Activities.findAll();
+
+        const activities = data.map((activity) => {
+            let dataActivities = activity.dataValues;
+            return {
+                ...dataActivities,
+                detail: `http://localhost:3000/api/activities/detail/${activity.id}`,
+            };
+        });
+        // Objeto que devuelve la API
+        res.json({
+            count: activities.length,
+            data: activities,
+            
+        });
+    },
+
+    async findActivity(req, res) {
+        const activity = await Activities.findByPk(req.params.id, {
+            include: ["images"],
+        });
+        console.log(activity.images);
+
+        const urlImages = activity.images.map((img) => {
+            return img.image;
+        });
+        // Objeto que devuelve la API
+        res.json({
+            activity: activity.dataValues,
+            urlImages
+        });
+    },
+};
