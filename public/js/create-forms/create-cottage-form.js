6361c82778eeca63626f0c409f9cf511f3a37b15
FedeVazquez <fedenv2009@hotmail.com>
2023-05-05 15:01:10 -0300

    Agrega botones para eliminar un servicio seleccionado en el formulario de creación de cabañas, y agrega el JS para que funcionen correctamente.

--- public/js/create-forms/create-cottage-form.js
+++ public/js/create-forms/create-cottage-form.js
@@ -1,11 +1,9 @@
 // Capturamos el option que muestra los servicios,
 // y el input que va a mostrar los seleccionados al usuario.
-
 const selectServices = document.getElementById("services-select");
 const selectedServices = document.getElementById("services-selected");
 
 // Captura el ul donde se mostrarán los servicios.
-
 const servicesList = document.getElementById("show-services");
 
 // Cuando seleccionemos una opción en "selectServices"
@@ -20,9 +18,47 @@ selectServices.addEventListener("change", (e) => {
     // Elimina la opción en "selectServices"
     selectServices.remove(selectServices.selectedIndex);
 
-    // Agrega un elemento al listado de "show-services"
-    servicesList.innerHTML += `<li class="w-50"><div class="selected-service">${valueSelected}<i class="fa-solid fa-xmark"></i></div></li>`;
+    // Agrega un elemento al listado de "servicesList"
+    servicesList.innerHTML += `
+    <li class="w-50">
+        <div class="selected-service">
+            ${valueSelected}
+            <i 
+                id= "${valueSelected}"
+                class="delete-service-button fa-solid fa-xmark"
+            >
+            </i>
+        </div>
+    </li>`;
 
     // Vuelve al valor "servicio"
     selectServices.value = "servicios";
+
+    // Capturamos los botones para eliminar un servicio ya seleccionado
+    const deleteServiceButtons = document.querySelectorAll(
+        ".delete-service-button"
+    );
+
+    // Le agregamos un evento a cada botón de eliminar.
+    deleteServiceButtons.forEach((deleteButton) => {
+        deleteButton.addEventListener("click", (e) => {
+            // Tomamos el li que queremos eliminar de "servicesList"
+            const liToDelete = deleteButton.parentElement.parentElement;
+            // Tomamos el id del botón, que es lo mismo que tiene el "value" del option que apretamos.
+            const valueLi = deleteButton.id;
+            // capturamos el option que estamos buscando
+            const optionToEliminate = document.querySelector(
+                `option[value="${valueLi}"]`
+            );
+
+            // Eliminamos ambos.
+            servicesList.removeChild(liToDelete);
+            selectedServices.remove(optionToEliminate.selectedIndex);
+
+            // Agregamos nuevamente la opción a "selectServices"
+            selectServices.innerHTML += `<option value="${valueLi}">${valueLi}</option>`;
+        });
+    });
+
+    // Lo volvemos a agregar al selected de "selectServices".
 });
