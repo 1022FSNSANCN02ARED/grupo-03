1ba458b2f6ab43742b1c461adfe42f87ff8363cd
FedeVazquez <fedenv2009@hotmail.com>
2023-05-06 21:08:53 -0300

    Modifica el controlador de productos, para que al cargar la vista carguen los valores que devuelve el middleware de errores. Y modifica el if ternario en el middleware de errores.

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -1,5 +1,14 @@
 module.exports = {
     showForm: (req, res) => {
-        res.render("create-product-form");
+        const formType = req.session.formType;
+        const errores = req.session.errores;
+        const oldData = req.session.oldData;
+        req.session.formType = null;
+        req.session.errores = null;
+        req.session.oldData = null;
+
+        console.log(formType);
+
+        res.render("create-product-form", { formType, errores, oldData });
     },
 };
--- src/middlewares/error_product_form.js
+++ src/middlewares/error_product_form.js
@@ -5,8 +5,7 @@ module.exports = (req, res, next) => {
 
     if (!errores.isEmpty()) {
         // "max_place" es un input único del formulario de actividad.
-        req.session.formType = req.body.max_place ? "activity" : "cottage";
-
+        req.session.formType = "max_place" in req.body ? "activity" : "cottage";
         req.session.errores = errores.mapped();
         req.session.oldData = req.body;
         res.redirect("/products/create");
