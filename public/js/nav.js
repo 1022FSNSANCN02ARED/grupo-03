ae050cafd8380b79c790c7349d295616fe762ccb
FedeVazquez <fedenv2009@hotmail.com>
2023-05-13 00:42:14 -0300

    Agrega funcionalidad al menú hamburguesa del nav.

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,14 +1,45 @@
 const userMenu = document.querySelector(".user-menu");
 
-userMenu.addEventListener("click", function () {
-    const userMenuOptions = document.querySelector(".user-menu-options");
+if (userMenu) {
+    userMenu.addEventListener("click", function () {
+        const userMenuOptions = document.querySelector(".user-menu-options");
+        if (
+            userMenuOptions.style.height == "0px" ||
+            !userMenuOptions.style.height
+        ) {
+            userMenuOptions.style.height = "auto";
+        } else {
+            userMenuOptions.style.height = "0px";
+        }
+    });
+}
+
+const burguerMenu = document.querySelector(".burguer-menu");
+
+burguerMenu.addEventListener("click", function () {
+    const burguerMenuOptions = document.querySelector(".burguer-menu-options");
     if (
-        userMenuOptions.style.height == "0px" ||
-        !userMenuOptions.style.height
+        burguerMenuOptions.style.height == "0px" ||
+        !burguerMenuOptions.style.height
     ) {
-        userMenuOptions.style.height = "auto";
-        arrow.style.rotate = "90px";
+        burguerMenuOptions.style.height = "auto";
     } else {
-        userMenuOptions.style.height = "0px";
+        burguerMenuOptions.style.height = "0px";
+    }
+});
+
+const burguerUserMenu = document.querySelector(".burguer-user-menu");
+
+burguerUserMenu.addEventListener("click", function () {
+    const burguerUserMenuOptions = document.querySelector(
+        ".burguer-user-menu-options"
+    );
+    if (
+        burguerUserMenuOptions.style.height == "0px" ||
+        !burguerUserMenuOptions.style.height
+    ) {
+        burguerUserMenuOptions.style.height = "auto";
+    } else {
+        burguerUserMenuOptions.style.height = "0";
     }
 });
