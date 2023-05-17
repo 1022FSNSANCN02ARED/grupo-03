ecb0f236743541a6f7c063353f93dc9a90889779
FedeVazquez <fedenv2009@hotmail.com>
2023-05-17 18:29:48 -0300

    Agrega mensaje de error en el comopnente de tarjetas.

--- dashboard/src/components/Tarjet.jsx
+++ dashboard/src/components/Tarjet.jsx
@@ -38,66 +38,72 @@ function Tarjet() {
     }
 
     return (
-        <div className="row mt-5 justify-content-center">
-            <div className="mb-4 " style={{ maxWidth: "500px" }}>
-                <div className="card border-left-primary shadow h-100 p-2">
-                    <div className="card-body">
-                        <div className="row no-gutters align-items-center">
-                            <div className="col mr-2">
-                                <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                    Cantidad De Usuario
+        <div>
+            {activities && users && cottages ? (
+                <div className="row mt-5 justify-content-center">
+                    <div className="mb-4 " style={{ maxWidth: "500px" }}>
+                        <div className="card border-left-primary shadow h-100 p-2">
+                            <div className="card-body">
+                                <div className="row no-gutters align-items-center">
+                                    <div className="col mr-2">
+                                        <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                            Cantidad De Usuario
+                                        </div>
+                                        <div className="h5 mb-0 font-weight-bold text-gray-800">
+                                            {users.count}
+                                        </div>
+                                    </div>
+                                    <div className="col-auto">
+                                        <i className="fas fa-user fa-2x text-gray-300"></i>
+                                    </div>
                                 </div>
-                                <div className="h5 mb-0 font-weight-bold text-gray-800">
-                                    {users.count}
-                                </div>
-                            </div>
-                            <div className="col-auto">
-                                <i className="fas fa-user fa-2x text-gray-300"></i>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
 
-            <div className="mb-4 " style={{ maxWidth: "500px" }}>
-                <div className="card border-left-success shadow h-100 p-2">
-                    <div className="card-body">
-                        <div className="row no-gutters align-items-center">
-                            <div className="col mr-2">
-                                <div className="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                    Cantidad De Cabañas
+                    <div className="mb-4 " style={{ maxWidth: "500px" }}>
+                        <div className="card border-left-success shadow h-100 p-2">
+                            <div className="card-body">
+                                <div className="row no-gutters align-items-center">
+                                    <div className="col mr-2">
+                                        <div className="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                            Cantidad De Cabañas
+                                        </div>
+                                        <div className="h5 mb-0 font-weight-bold text-gray-800">
+                                            {cottages.count}
+                                        </div>
+                                    </div>
+                                    <div className="col-auto">
+                                        <i className="fas fa-award fa-2x text-gray-300"></i>
+                                    </div>
                                 </div>
-                                <div className="h5 mb-0 font-weight-bold text-gray-800">
-                                    {cottages.count}
-                                </div>
-                            </div>
-                            <div className="col-auto">
-                                <i className="fas fa-award fa-2x text-gray-300"></i>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
 
-            <div className="mb-4 " style={{ maxWidth: "500px" }}>
-                <div className="card border-left-warning shadow h-100 p-2">
-                    <div className="card-body">
-                        <div className="row no-gutters align-items-center">
-                            <div className="col mr-2">
-                                <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                    Cantidad De Actividades
+                    <div className="mb-4 " style={{ maxWidth: "500px" }}>
+                        <div className="card border-left-warning shadow h-100 p-2">
+                            <div className="card-body">
+                                <div className="row no-gutters align-items-center">
+                                    <div className="col mr-2">
+                                        <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                            Cantidad De Actividades
+                                        </div>
+                                        <div className="h5 mb-0 font-weight-bold text-gray-800">
+                                            {activities.count}
+                                        </div>
+                                    </div>
+                                    <div className="col-auto">
+                                        <i className=" fa-2x text-gray-300"></i>
+                                    </div>
                                 </div>
-                                <div className="h5 mb-0 font-weight-bold text-gray-800">
-                                    {activities.count}
-                                </div>
-                            </div>
-                            <div className="col-auto">
-                                <i className=" fa-2x text-gray-300"></i>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            ) : (
+                <p>error en el servidor</p>
+            )}
         </div>
     );
 }
