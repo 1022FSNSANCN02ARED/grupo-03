370fc99993c5358127bcd7e85b72887fb1334b1b
FedeVazquez <fedenv2009@hotmail.com>
2023-05-16 23:07:56 -0300

    Crea componentes de actividades, para manejar las rutas de las mismas, para cargar la vista que tendrá a todos los usuarios (sin implementar de momento), y crea el componente para ver la última activiad cargada.

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import "./App.css";
 import Cottages from "./components/Cottages";
 import NavBar from "./components/NavBar";
-import MainUsersSection from "./components/UsersSection/MainUsersSection";
 import { Route, Routes } from "react-router-dom";
 import Tarjet from "./components/Tarjet";
+
+import MainUsersSection from "./components/UsersSection/MainUsersSection";
+import MainActivitiesSection from "./components/Products/activities/MainActivitiesSection";
 
 function App() {
     return (
@@ -13,6 +15,11 @@ function App() {
             <Routes>
                 <Route exact path="/" Component={Tarjet} />
                 <Route exact path="/users/*" Component={MainUsersSection} />
+                <Route
+                    exact
+                    path="/activities/*"
+                    Component={MainActivitiesSection}
+                />
             </Routes>
 
             <header className="App-header">
---
+++ dashboard/src/components/Products/activities/AllActivites.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import LastActivity from "./LastActivity";
+
+function AllUsersSection() {
+    const [activities, setActivities] = useState(null);
+    const [isLoading, setisLoading] = useState(true);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/api/activities")
+            .then((response) => {
+                return response.json();
+            })
+            .then((result) => {
+                setisLoading(false);
+                setActivities(result);
+            })
+            .catch((error) => {
+                setisLoading(false);
+                console.log(error);
+            });
+    }, []);
+
+    if (isLoading) {
+        return <p>cargando...</p>;
+    }
+
+    return (
+        <div className="container-xl">
+            {activities && (
+                <LastActivity
+                    lastActivity={activities.data[activities.count - 1]}
+                />
+            )}
+            {/* {activities && <UsersList users={activities.data} />} */}
+        </div>
+    );
+}
+
+export default AllUsersSection;
